6b3f067401870ea7e8be14aa0e9fdf64b29e63b2
Aram Hammoudeh <aramcontact@gmail.com>
2018-05-21 13:26:42 -0600

    Finished with day one, page working

--- server.js
+++ server.js
@@ -7,7 +7,35 @@ const data = require('./db/notes');
 const app = express();
 
 // ADD STATIC SERVER HERE
+
+//Pull files from public and push to DOM
 app.use(express.static('public'));
+
+//Endpoint to return a list of notes
+app.get('/api/notes', (req, res) => {
+
+  const searchTerm = req.query.searchTerm;
+  if (searchTerm) {
+    let filteredList = data.filter(function (item) {
+      return item.title.includes(searchTerm);
+    });
+    res.json(filteredList);
+  } else {
+    res.json(data);
+  }
+
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+
+
+  let note = data.find(function (item) {
+    return item.id === Number(id);
+  });
+  res.json(note);
+
+});
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
